63925a1c81bfec006d33b14383bf45922d4136a4
thyagolima23 <thyago.lima2324@gmail.com>
2025-02-27 22:17:23 -0300

    dados

---
+++ dados.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        "dia": "segunda-feira",
+        "cardapio": "feijoada, arroz, farofa, couve, laranja",
+        "img": "https://www.orlario.com.br/wp-content/uploads/2020/10/Feijoada-1.jpg",
+        "alt": "prato com feijoada",
+    },
+    {
+        "dia": "terca-feira",
+        "cardapio": "arroz, macarrao, alface, tomate, beterraba, ",
+        "img": "https://scontent.fbel12-1.fna.fbcdn.net/v/t39.30808-6/428386828_781067404053440_906121552512598658_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=e4kcuGdg6-kQ7kNvgGIEHNP&_nc_oc=Adi9oJgZUl4IsHBjCLHsMAZxo8XYVbXMDJDvrdlJlbQgelMAEhKVzjqfkFWXNkHM03IfxEBjYVBQGzSl0i51vkBb&_nc_zt=23&_nc_ht=scontent.fbel12-1.fna&_nc_gid=Ah7TwcUO3yMXZLMOOW4rn_U&oh=00_AYAh4YUiRT5o-fbfc5snWv2orD2lBgiBcdPO5Q8PzyufSg&oe=67C6D21A",
+        "alt": "prato com arroz, macarrao e salada",
+    },
+    {
+        "dia": "quarta-feira",
+        "cardapio": "feijao, arroz, frango, cenoura, suco",
+        "img": "https://i.pinimg.com/736x/91/b1/c2/91b1c21271eab240294a15b0837b0c51.jpg",
+        "alt": "prato com feijao, arroz, frango e suco",
+    },
+    {
+        "dia": "quinta-feira",
+        "cardapio": "strogonoff de carne, feijao, arroz, banana, batata",
+        "img": "https://img.cybercook.com.br/receitas/30/strogonoff-pratico-de-frango-e-arroz-840x480.jpeg?q=75",
+        "alt": "strogonof acompanhado de arroz e batata",
+    },
+    {
+        "dia": "sexta-feira",
+        "cardapio": "tutu, repolho, arroz, ovo, melancia",
+        "img": "https://as2.ftcdn.net/jpg/04/59/99/43/1000_F_459994397_xF9lDvIlCDxjqRjY3jwfc3MP1PtxOvlt.webp",
+        "alt": "arroz com tutu e ovo",
+    },
+    {
+        "dia": "sabado",
+        "cardapio": "lanchao",
+        "img": "https://catracalivre.com.br/wp-content/uploads/2020/10/lanchao-de-forno1-ct-910x512.jpg",
+        "alt": "lanchao de forno",
+    }
+]
